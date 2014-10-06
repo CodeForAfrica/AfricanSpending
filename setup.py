da4583ae60a99ac71af2fce0d6760b3221f22a76
Friedrich Lindenberg <friedrich@pudo.org>
2014-10-06 14:24:24 +0200

    Begin transforming this into a Python site.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='africanspending',
+    version='1.0',
+    description="Mapping the money, across the African continent",
+    long_description='',
+    classifiers=[],
+    keywords='',
+    author='Code for Africa',
+    author_email='friedrich@pudo.org',
+    url='http://www.africanspending.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points="",
+)
