3b2454b08c91e418367bfd7df4f316efc2aaa65e
Friedrich Lindenberg <friedrich@pudo.org>
2014-10-08 16:21:42 +0200

    begin importing IBP data.

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 web:
 	python africanspending/manage.py runserver
+
+
+countries:
+	wget -O contrib/obs.json http://obstracker.internationalbudget.org/countries.json
+	python contrib/convert_countries.py
--- contrib/convert_countries.py
+++ contrib/convert_countries.py
@@ -1,5 +1,7 @@
-import yaml
+import os
+import yaml, json
 import unicodecsv
+from pprint import pprint
 
 COUNTRIES = ['za', 'dz', 'ao', 'bz', 'bw', 'bf', 'bi',
              'cm', 'cf', 'td', 'cg', 'cd', 'dj', 'eg',
@@ -12,7 +14,9 @@ COUNTRIES = ['za', 'dz', 'ao', 'bz', 'bw', 'bf', 'bi',
 
 print len(set(COUNTRIES))
 
-out_file = '../data/library/countries.yaml'
+my_path = os.path.dirname(__file__)
+
+out_file = os.path.join(my_path, '../data/library/countries.yaml')
 
 try:
     out = yaml.load(open(out_file, 'rb').read())
@@ -20,7 +24,7 @@ except Exception, e:
     print e
     out = {}
 
-with open('countries.csv', 'r') as fh:
+with open(os.path.join(my_path, 'countries.csv'), 'r') as fh:
     reader = unicodecsv.DictReader(fh)
     for row in reader:
         if row.get('linked_country'):
@@ -39,6 +43,14 @@ with open('countries.csv', 'r') as fh:
             if k not in od:
                 od[k] = v
         out[key] = od
+
+for cd in json.load(open(os.path.join(my_path, 'obs.json'), 'rb')):
+    key = cd['code'].lower()
+    if key not in out:
+        continue
+    pprint(cd)
+    out[key]['obi'] = cd.get('obi_scores')
+    out[key]['ibp_library'] = cd.get('library')
 
 
 print 'missing', set(COUNTRIES) - set(out.keys())
