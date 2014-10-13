e04ecb603a85513032c855cf20c367574bea2f09
Friedrich Lindenberg <friedrich@pudo.org>
2014-10-13 18:32:20 +0200

    Little maps on the country pages

--- africanspending/static/js/map.js
+++ africanspending/static/js/map.js
@@ -1,6 +1,7 @@
 $(function() {
     var $map = $('#map'),
-        mapLinks = $map.data('map-links');
+        mapLinks = $map.data('map-links'),
+        mapSelect = $map.data('map-select');
 
     var drawMap = function($dfd) {
         var width = $map.width(),
@@ -25,7 +26,13 @@ $(function() {
             svg.selectAll(".country")
                     .data(countries.features)
                 .enter().append("path")
-                    .attr("class", function(d) { return "country " + d.id;})
+                    .attr("class", function(d) {
+                        var clazz = "country " + d.id;
+                        if (d.id == mapSelect) {
+                            clazz = "selected " + clazz;
+                        }
+                        return clazz;
+                    })
                     .attr("d", path)
                     .on("click", function(d) {
                         var path = mapLinks[d.id];
@@ -35,27 +42,31 @@ $(function() {
                     })
                     .on("mouseenter", function(d) {
                         var self = d3.select(this);
-                        self.attr('class', 'hovered country ' + d.id);
+                        d.baseClass = self.attr('class');
+                        self.attr('class', d.baseClass + ' hovered');
                     })
                     .on("mouseleave", function(d) {
                         var self = d3.select(this);
-                        self.attr('class', 'country ' + d.id);  
+                        self.attr('class', d.baseClass);  
                     });
 
-            svg.append("path")
-                .datum(topojson.feature(mapData, mapData.objects.places))
-                .attr("d", path)
-                .attr("class", "place");
+            if (!mapSelect) {
+                svg.append("path")
+                    .datum(topojson.feature(mapData, mapData.objects.places))
+                    .attr("d", path)
+                    .attr("class", "place");
 
-            svg.selectAll(".place-label")
-                    .data(topojson.feature(mapData, mapData.objects.places).features)
-                .enter().append("text")
-                    .attr("class", "place-label")
-                    .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-                    .attr("dy", ".35em")
-                    .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
-                    .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
-                    .text(function(d) { return d.properties.name; });
+                svg.selectAll(".place-label")
+                        .data(topojson.feature(mapData, mapData.objects.places).features)
+                    .enter().append("text")
+                        .attr("class", "place-label")
+                        .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+                        .attr("dy", ".35em")
+                        .attr("x", function(d) { return d.geometry.coordinates[0] > -1 ? 6 : -6; })
+                        .style("text-anchor", function(d) { return d.geometry.coordinates[0] > -1 ? "start" : "end"; })
+                        .text(function(d) { return d.properties.name; });    
+            }
+            
         });
     };
 
